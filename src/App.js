f134a7ca853b31eb1df82bfcbe6cfdb9bd38340b
George Ciocan <george.ciocan21@gmail.com>
2020-05-29 13:23:23 +0200

    Added Login form

--- src/App.js
+++ src/App.js
@@ -51,6 +51,11 @@ function Home() {
   return <h2>Home</h2>;
 }
 
+
+
+
+
+
 function Signup() {
 
   const capitalLetter = /([A-Z])+/g;
@@ -77,22 +82,21 @@ function Signup() {
       user
     }
 
-      try {
-        const result = await axios.post("https://conduit.productionready.io/api/users", payload)
-        console.log(result.data)
-      } catch (error) {
-        let errorNames = Object.keys(error.response.data.errors)
-        let errorObject = {
-          username: "",
-          email: "",
-          password: ""
-        };
-        errorNames.forEach(name => errorObject[name] = error.response.data.errors[name])
-        console.log(errorObject)
-        setSubmitError(errorObject) 
-      }      
+    try {
+      const result = await axios.post("https://conduit.productionready.io/api/users", payload)
+      console.log(result.data)
+    } catch (error) {
+      let errorNames = Object.keys(error.response.data.errors)
+      let errorObject = {
+        username: "",
+        email: "",
+        password: ""
+      };
+      errorNames.forEach(name => errorObject[name] = error.response.data.errors[name])
+      console.log(errorObject)
+      setSubmitError(errorObject) 
+    }      
   }
-  console.log(submitError)
 
   const handleSubmitError = (name) => {
     if (submitError.hasOwnProperty("username") && name ==="username") {
@@ -130,14 +134,73 @@ function Signup() {
   )
 }
 
+
+
+
+
+
 function Login() {
-  return <h2>Login</h2>;
+
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [submitError, setSubmitError] = useState("") 
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const payload = {
+      user
+    }
+    console.log(payload.user)
+    try {
+      const result = await axios.post("https://conduit.productionready.io/api/users/login", payload)
+      console.log(result.data)
+    } catch (error) {
+      let errorObject = error.response.data.errors
+      let errorField = Object.keys(errorObject)[0]
+      let errorMessage = errorObject[errorField]
+      let message = errorField + " " + errorMessage[0]
+      console.log(message)
+      setSubmitError(message)
+    }
+  }
+
+
+
+  const handleChange = (event) => {
+    setUser({...user, [event.target.name]: event.target.value})
+    setSubmitError("")
+  }
+
+  return (
+    <div className="login">
+    <header>
+      <h1>Login</h1>
+      <Link to="/signup">Don't have an account?</Link>
+    </header>
+    <main>
+    <form onSubmit={handleSubmit}>
+      <FormInput label="Email" name="email" type="email" required={true} onValueChange={handleChange} value={user.email} submitError={submitError} />
+      <FormInput label="Password" name="password" type="password" required={true} onValueChange={handleChange} value={user.password} submitError={submitError} />
+      <button type="submit">Submit</button>
+    </form>
+    </main>
+    </div>
+  )
 }
 
 
-const FormInput = ({ label, name, type, placeholder, required, minLength, match, onValueChange, value, submitError }) => {
 
-  const [error, setError] = useState();
+
+
+
+
+const FormInput = ({ label, name, type, placeholder, required, onValueChange, value, submitError }) => {
+
+  const [error, setError] = useState("");
 
   const handleError = () => {
     if (required) {
@@ -147,36 +210,6 @@ const FormInput = ({ label, name, type, placeholder, required, minLength, match,
       } else {
         setError("")
       } 
-    } 
-    if (minLength) {
-      if (value.length < minLength) {
-      setError(`You need to enter at least ${minLength} characters`)
-      return
-      } else {
-        setError("")
-      } 
-    }
-    if(match) {
-        if(!value.match(match) && type==="email") {
-          setError("You must enter a valid email address")
-          return
-        } else {
-          setError("")
-        }      
-        if (type==="password" ) {
-          if (!value.match(match[0])) {
-          setError("The password must contain at least one capital letter")
-          return
-        } if (!value.match(match[1])) {
-          setError("The password must contain at least one small letter")
-          return
-        } else if (!value.match(match[2])) {
-          setError("The password must contain at least one digit")
-          return
-        } else {
-          setError("")
-        }
-      }
     }
   }
 
@@ -198,5 +231,8 @@ const FormInput = ({ label, name, type, placeholder, required, minLength, match,
   </Fragment>
   )
 }
+
+
+
 
 export default App;
